e556aab36c3e987310ce3d7664d8d4e8ad79dcca
leroment <andrewcai19972011@gmail.com>
2018-09-02 23:34:44 +1000

    implement routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { RouterModule } from '@angular/router'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { appRoutes } from './routes'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
