0359943a6d36540d448ef6ae5a8084ca87520a91
leroment <andrewcai19972011@gmail.com>
2018-11-11 16:23:27 +1100

    implment router link for products

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,9 +4,11 @@ import { CustomerListResolver } from "./shared/services/customer-list-resolver.s
 import { ProductListResolver } from "./shared/services/product-list-resolver.service";
 import { OrderItemListResolver } from "./shared/services/order-item-list-resolver.service";
 import { OrderListResolver } from "./shared/services/order-list-resolver.service";
+import { ProductsComponent } from "./products/products.component";
 
 export const appRoutes : Routes = [
     { path: 'dashboard', component: DashboardComponent, resolve: { customers: CustomerListResolver, products: ProductListResolver, 
         orderItems: OrderItemListResolver, orders: OrderListResolver} },
+    { path: 'products', component: ProductsComponent},
     { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ]
