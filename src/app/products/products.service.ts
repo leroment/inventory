fe6073d0ec9be1380d99dd5ee516ae549ed4e281
leroment <andrewcai19972011@gmail.com>
2018-11-18 21:07:04 +1100

    added httpget for products

---
+++ src/app/products/products.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
+import { IProduct } from '../shared/models/inventory.model';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  baseUrl: string = '/api/products';
+
+  constructor(private http: Http) { }
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get(this.baseUrl)
+      .pipe(map((res: Response) => {
+        let products = res.json();
+        return products;
+      }),
+      catchError(this.handleError));
+  }
+
+  getProduct(id: string): Observable<IProduct> {
+    return this.http.get(this.baseUrl + '/' + id)
+      .pipe(map((res: Response) => res.json()),
+      catchError(this.handleError));
+  }
+
+  private handleError(error: any) {
+    console.error('server error:', error);
+    if (error instanceof Response)
+    {
+      let errMessage = '';
+      try {
+        errMessage = error.json().error;
+      } catch(err) {
+        errMessage = error.statusText;
+      }
+      return Observable.throw(errMessage);
+    }
+    return Observable.throw(error || 'ASP.NET Core server error');
+  }
+}
